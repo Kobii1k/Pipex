c9031032e4a91ae59ce63cce6f1f2b1075355a78
Maxens Gagne <mgagne@z3r2p1.42lyon.fr>
2023-05-09 15:45:19 +0200

    lib update

--- pipex.h
+++ pipex.h
@@ -6,14 +6,14 @@
 /*   By: mgagne <mgagne@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/02 16:34:34 by mgagne            #+#    #+#             */
-/*   Updated: 2023/04/24 14:11:38 by mgagne           ###   ########.fr       */
+/*   Updated: 2023/05/05 17:11:41 by mgagne           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
 
-# include "big_Libft/libft.h"
+# include "big_libft/libft.h"
 # include <stdlib.h>
 # include <unistd.h>
 # include <fcntl.h>
